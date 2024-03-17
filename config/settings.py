06992d08d94eb949f6fbf86fbf7923cff729227e
EngRobot33 <hmdkhsrvee@gmail.com>
2024-03-17 22:03:15 +0330

    [ADD] request and media context processors of django template

--- config/settings.py
+++ config/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
+                'django.template.context_processors.media',
             ],
         },
     },
