36d347c6c932042a5caa96946d5618c99f3b9d39
Mohammadreza <mohammadreza.mrn.29@gmail.com>
2024-03-21 19:37:16 +0330

    disable secret key and debug mode

--- config/settings.py
+++ config/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', True)
+SECRET_KEY = config('SECRET_KEY', False)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', True)
+DEBUG = config('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
