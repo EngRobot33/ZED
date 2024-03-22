0a58391429f6dc9721e899d97ce41d8013eb59dc
EngRobot33 <hmdkhsrvee@gmail.com>
2024-03-22 19:49:51 +0330

    [STYLE] PEP8 format

--- config/middlewares/rate_limiter.py
+++ config/middlewares/rate_limiter.py
@@ -1,12 +1,16 @@
+from typing import Any
+
+from django.http import HttpResponse
+
 from utils.rate_limiter_handler import RateLimiterHandler
+
 
 class RateLimiterMiddleware:
 
-    def __init__(self,get_response) -> None:
+    def __init__(self, get_response) -> None:
         self.get_response = get_response
 
-    def __call__(self,request) -> None:
-
+    def __call__(self, request) -> HttpResponse | Any:
         response = RateLimiterHandler.do_handler(request)
 
         if response is not None:
--- utils/rate_limiter_handler.py
+++ utils/rate_limiter_handler.py
@@ -5,26 +5,26 @@ from .redis_manager import RedisHandler
 
 import json
 
-redis_context = RedisHandler("localhost",6379,0)
+redis_context = RedisHandler("localhost", 6379, 0)
 redis_context.connect()
 
+
 class RateLimiterHandler:
+    USER_INFO_KEY: str = "user_{0}"
+    USER_REQUEST_COUNT: str = "user_reqcount_{0}"
+    USER_LAST_REQUEST_TIME: str = "user_lastreqtime_{0}"
 
-    USER_INFO_KEY:str = "user_{0}"
-    USER_REQUEST_COUNT:str = "user_reqcount_{0}"
-    USER_LAST_REQUEST_TIME:str = "user_lastreqtime_{0}"
+    MESSAGE_TOO_MANY_REQUESTS: str = "you have sent too many requests , please try again in a minute later"
 
-    MESSAGE_TOO_MANY_REQUESTS:str = "you have sent too many requests , please try again in a minute later"
+    redis_connection_error_counter: int = 0
 
-    redis_connection_error_counter:int = 0
-
-    REQUEST_COUNT:int = 10
-    EACH_SECONDS:int = 60
+    REQUEST_COUNT: int = 10
+    EACH_SECONDS: int = 60
 
     @classmethod
-    def do_handler(cls,request) -> None | HttpResponse:
+    def do_handler(cls, request) -> None | HttpResponse:
 
-        client_ip:str = cls.__get_client_ip(request)
+        client_ip: str = cls.__get_client_ip(request)
         try:
             ip = redis_context.redis_client.get(cls.USER_INFO_KEY.format(client_ip))
 
@@ -32,17 +32,17 @@ class RateLimiterHandler:
 
                 now = datetime.now()
 
-                set_time:bytes = redis_context.redis_client.get(cls.USER_LAST_REQUEST_TIME.format(client_ip))
+                set_time: bytes = redis_context.redis_client.get(cls.USER_LAST_REQUEST_TIME.format(client_ip))
                 set_time = set_time.decode()
-                set_time = datetime.strptime(set_time,"%Y-%m-%d %H:%M:%S.%f")
+                set_time = datetime.strptime(set_time, "%Y-%m-%d %H:%M:%S.%f")
 
-                if cls.__check_time(now,set_time):
-                    request_count:bytes = redis_context.redis_client.get(cls.USER_REQUEST_COUNT.format(client_ip))
+                if cls.__check_time(now, set_time):
+                    request_count: bytes = redis_context.redis_client.get(cls.USER_REQUEST_COUNT.format(client_ip))
                     request_count = int(request_count.decode())
 
                     if request_count > cls.REQUEST_COUNT:
-                        content:str = json.dumps({"message" : cls.MESSAGE_TOO_MANY_REQUESTS})
-                        return HttpResponse(content=content,status=403)
+                        content: str = json.dumps({"message": cls.MESSAGE_TOO_MANY_REQUESTS})
+                        return HttpResponse(content=content, status=403)
                     redis_context.redis_client.incr(cls.USER_REQUEST_COUNT.format(client_ip))
 
                 else:
@@ -50,17 +50,18 @@ class RateLimiterHandler:
                     redis_context.redis_client.delete(cls.USER_REQUEST_COUNT.format(client_ip))
                     redis_context.redis_client.delete(cls.USER_LAST_REQUEST_TIME.format(client_ip))
             else:
-                redis_context.redis_client.set(cls.USER_INFO_KEY.format(client_ip),client_ip)
-                redis_context.redis_client.set(cls.USER_REQUEST_COUNT.format(client_ip),0)
-                redis_context.redis_client.set(cls.USER_LAST_REQUEST_TIME.format(client_ip),str(datetime.now()))
+                redis_context.redis_client.set(cls.USER_INFO_KEY.format(client_ip), client_ip)
+                redis_context.redis_client.set(cls.USER_REQUEST_COUNT.format(client_ip), 0)
+                redis_context.redis_client.set(cls.USER_LAST_REQUEST_TIME.format(client_ip), str(datetime.now()))
         except ConnectionError:
             ...
+
     @classmethod
-    def __check_time(cls,now:datetime,set_time:datetime) -> bool:
+    def __check_time(cls, now: datetime, set_time: datetime) -> bool:
         return (now - set_time).seconds < cls.EACH_SECONDS
-    
+
     @classmethod
-    def __get_client_ip(cls,request) -> str:
+    def __get_client_ip(cls, request) -> str:
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
             ip = x_forwarded_for.split(',')[0]
