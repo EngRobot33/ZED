5790003b2e0edf8cb50de6c2f99f4bbe86f71daf
Mohammadreza <mohammadreza.mrn.29@gmail.com>
2024-03-24 21:28:15 +0330

    implement websocket protocol

--- config/asgi.py
+++ config/asgi.py
@@ -8,9 +8,27 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","config.settings")
+django.setup()
+
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+from chat.router import chat_websocket_urlpatterns
 
-application = get_asgi_application()
+from chat.middlewares import TokenAuthenticationMiddleWare
+
+django_asgi_app = get_asgi_application()
+
+
+application = ProtocolTypeRouter({
+    "http" : django_asgi_app,
+    "websocket" : AllowedHostsOriginValidator([
+        TokenAuthenticationMiddleWare(
+            URLRouter(chat_websocket_urlpatterns)
+        )
+    ])
+})
