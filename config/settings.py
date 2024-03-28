b9a305ba50ff68d99a2b01463cd99d371a050e52
EngRobot33 <hmdkhsrvee@gmail.com>
2024-03-29 00:12:15 +0330

    [FEAT] use PostgreSQL instead of SQLite for database

--- config/settings.py
+++ config/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
