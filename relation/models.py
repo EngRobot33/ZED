d2de97ed348958a6f4b5bce330bcd28c40b7ff7d
erfan baghi <er.baghi@gmail.com>
2024-03-28 18:31:57 +0330

    changes on relation.Relation model

--- relation/models.py
+++ relation/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -12,17 +13,17 @@ class Relation(BaseModel):
         User,
         verbose_name=_('following'),
         on_delete=models.CASCADE,
+        related_name='followers',
         blank=True,
         null=True,
-        related_name='following'
     )
     follower = models.ForeignKey(
         User,
         verbose_name=_('follower'),
         on_delete=models.CASCADE,
+        related_name='following_users',
         blank=True,
         null=True,
-        related_name='follower'
     )
 
     class Meta:
@@ -32,3 +33,7 @@ class Relation(BaseModel):
 
     def __str__(self):
         return f'{self.follower} -> {self.following}'
+
+    def clean(self):
+        if self.following == self.follower:
+            raise ValidationError("A user cannot follow themselves.")
